844c4a95f3a6b7cfaee4500319c7bc1e9388b803
Sandesh Khadka <ksandesh283@gmail.com>
2023-11-14 20:17:48 +0545

    added title content

---
+++ src/components/FamilyGeneration.js
@@ -0,0 +1,14 @@
+// src/components/FamilyGeneration.js
+import React from 'react';
+
+const FamilyGeneration = () => {
+  return (
+    <div>
+      {/* Family Generation content */}
+      <h1>Select Family Generation</h1>
+      {/* Add your selection elements here */}
+    </div>
+  );
+};
+
+export default FamilyGeneration;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+// src/components/Navbar.js
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      {/* Navbar content */}
+      <ul>
+        <li>Home</li>
+        {/* Add more navbar items */}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
